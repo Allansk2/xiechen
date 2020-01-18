21f5214de8197dba9dc112635f6c408256a8122b
Allansk2 <zangen2010@gmail.com>
2020-01-18 14:34:36 -0500

    Add data model Fetch data from server

---
+++ lib/model/home_model.dart
@@ -0,0 +1,46 @@
+import 'package:xiechen/model/common_model.dart';
+import 'package:xiechen/model/config_model.dart';
+import 'package:xiechen/model/grid_nav_model.dart';
+import 'package:xiechen/model/sales_box_model.dart';
+
+class HomeModel {
+  final ConfigModel config;
+  final List<CommonModel> bannerList;
+  final List<CommonModel> localNavList;
+  final GridNavModel gridNav;
+  final List<CommonModel> subNavList;
+  final SalesBoxModel salesBox;
+
+  HomeModel(
+      {this.bannerList,
+      this.config,
+      this.localNavList,
+      this.gridNav,
+      this.subNavList,
+      this.salesBox});
+
+  factory HomeModel.fromJson(Map<String, dynamic> json) {
+    var localNavListJson = json['localNavList'] as List;
+    List<CommonModel> localNavList =
+        localNavListJson.map((i) => CommonModel.fromJson(i)).toList();
+
+    var bannerListJson = json['bannerList'] as List;
+    List<CommonModel> bannerList =
+        bannerListJson.map((i) => CommonModel.fromJson(i)).toList();
+
+    var subNavListJson = json['subNavList'] as List;
+    List<CommonModel> subNavList =
+        subNavListJson.map((i) => CommonModel.fromJson(i)).toList();
+
+    return HomeModel(
+        config: ConfigModel.fromJson(json['config']),
+        bannerList: bannerList,
+        localNavList: localNavList,
+        gridNav: GridNavModel.fromJson(json['gridNav']),
+        subNavList: subNavList,
+        salesBox: SalesBoxModel.fromJson(json['salesBox']));
+  }
+
+
+
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:xiechen/dao/home_dao.dart';
+import 'package:xiechen/model/home_model.dart';
+import 'package:xiechen/widget/grid_nav.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,14 +13,22 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List _imageUrls = [
-    "http://personal.psu.edu/xqz5228/jpg.jpg",
-    "https://previews.123rf.com/images/evryka23/evryka231207/evryka23120700021/14405918-blue-background-with-butterfly.jpg",
-    "https://i.kym-cdn.com/photos/images/original/001/468/202/b02.jpg"
+    'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
+    'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg',
+    'https://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg'
   ];
 
   static const APPBAR_SCROLL_OFFSET = 200;
 
   double appBarAlpha = 0;
+
+  String resultString = '';
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
 
   void _onScroll(offset) {
     double alpha = offset / APPBAR_SCROLL_OFFSET;
@@ -29,6 +42,29 @@ class _HomePageState extends State<HomePage> {
       appBarAlpha = alpha;
     });
     print("alpha=$alpha");
+  }
+
+  loadData() async {
+//    HomeDao.fetch().then((result) {
+//      setState(() {
+//        resultString = json.encode(result);
+//      });
+//    }).catchError((e) {
+//      setState(() {
+//        resultString = e.toString();
+//      });
+//    });
+
+    try {
+      HomeModel mode = await HomeDao.fetch();
+      setState(() {
+        resultString = json.encode(mode.config);
+      });
+    } catch (e) {
+      setState(() {
+        resultString = e.toString();
+      });
+    }
   }
 
   @override
@@ -56,14 +92,15 @@ class _HomePageState extends State<HomePage> {
                         autoplay: true,
                         itemBuilder: (BuildContext context, int index) {
                           return Image.network(_imageUrls[index],
-                              fit: BoxFit.fitWidth);
+                              fit: BoxFit.fill);
                         },
                         pagination: SwiperPagination(),
                       ),
                     ),
+                    GridNav(gridNavModel: null, name: 'Allan'),
                     Container(
                       height: 1800,
-                      child: ListTile(title: Text("haha")),
+                      child: ListTile(title: Text(resultString)),
                     )
                   ],
                 ))),
